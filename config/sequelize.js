7af29efa551f7b967d15b364d59814b13c835b56
ppermadik <ppermadik195@gmail.com>
2023-10-05 21:50:22 +0800

    edit config/sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize=new Sequelize(
-    process.env.MARIADB_URL
+    process.env.MYSQL_URL
     // process.env.DB_NAME,
     // process.env.DB_USERNAME,
     // process.env.DB_PASSWORD,

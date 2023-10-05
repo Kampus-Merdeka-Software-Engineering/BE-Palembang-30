c29fe0e632711f7ba401c6c167595e5af5303d21
ppermadik <ppermadik195@gmail.com>
2023-10-06 00:47:04 +0800

    update sequelize.js in config

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,17 +1,21 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import mysql2 from 'mysql2';
 
 dotenv.config();
 
 const sequelize=new Sequelize(
-    process.env.MYSQL_URL
-    // process.env.DB_NAME,
-    // process.env.DB_USERNAME,
-    // process.env.DB_PASSWORD,
-    // {
-    //     host:process.env.DB_HOST,
-    //     dialect:process.env.DB_DIALECT
-    // }
+    // process.env.MYSQL_URL
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
+    {
+        dialect: 'mysql',
+        dialectModule: mysql2,
+        host:process.env.MYSQLHOST,
+        port: process.env.MYSQLPORT
+        // dialect:process.env.DB_DIALECT
+    }
 );
 
 export default sequelize;

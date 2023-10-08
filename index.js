1f3f8a5b0bb5c6447b7234946b0b18ac28bfe1b8
ppermadik <ppermadik195@gmail.com>
2023-10-08 18:29:18 +0800

    editing cors options

--- index.js
+++ index.js
@@ -29,10 +29,23 @@ startSequelize(sequelize);
 server.use(bodyParser.urlencoded({extended: false}));
 server.use(bodyParser.raw());
 server.use(bodyParser.json());
-server.use(cors({
-  origin: ['http://127.0.0.1:5501', 'https://be-palembang-30.vercel.app'],
-  credentials: true // Allow credentials (cookies, headers)
-}));
+const whitelist = ['http://127.0.0.1:5501', 'https://be-palembang-30.vercel.app'];
+
+// ✅ Enable pre-flight requests
+server.options('*', cors());
+
+const corsOptions = {
+  credentials: true,
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+server.use(cors(corsOptions));
 
 server.use(sessionMiddleware);
 
